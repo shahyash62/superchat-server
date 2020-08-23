5311c07653b533a530873b4c6164b0a427c206fa
shahyash62 <shahyash62@gmail.com>
2020-08-23 13:53:20 -0400

    added log for login

--- source/routes/login.ts
+++ source/routes/login.ts
@@ -9,15 +9,19 @@ const router = express();
 router.post('/', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password || '';
-    const auth = await authenticateUser(username, password);
-    if (!auth.errorCode) {
-        const token = getToken(username);
-        const [userData, userMessageQueue] = await Promise.all([findUserData(username), findUserMessageQueue(username)]);
-        res.append('authentication', token).json({ userData, userMessageQueue });
-        emptyQueue(userMessageQueue);
-        return;
-    } else {
-        res.status(401).json({ errorCode: auth.errorCode });
+    try {
+        const auth = await authenticateUser(username, password);
+        if (!auth.errorCode) {
+            const token = getToken(username);
+            const [userData, userMessageQueue] = await Promise.all([findUserData(username), findUserMessageQueue(username)]);
+            res.append('authentication', token).json({ userData, userMessageQueue });
+            emptyQueue(userMessageQueue);
+            return;
+        } else {
+            res.status(401).json({ errorCode: auth.errorCode });
+        }
+    } catch (error) {
+        console.log('login error: ', error);
     }
 });
 
